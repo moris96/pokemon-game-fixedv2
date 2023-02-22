9efeb0be18e683a8fb489660e4df9401fdf0d21d
moris96 <moriskhoudari@gmail.com>
2023-02-22 14:47:43 -0500

    loaded map onto canvas

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ canvas.width = 1024
 canvas.height = 576 
 ctx.fillStyle = 'white'
 ctx.fillRect(0,0,canvas.width,canvas.height)
+
+
+const image = new Image()
+image.src = './images/gameMap.png'
+ctx.drawImage(image, 0, 0)
+
+image.onload = () => {
+    ctx.drawImage(image, -500, -250)
+}
