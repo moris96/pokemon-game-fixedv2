a5ee23ecb044ff5985ef458f59e9e912d620455a
moris96 <moriskhoudari@gmail.com>
2023-02-23 10:36:17 -0500

    testing collision detection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// import { collisions } from "./collisions/collisions";
+// import { collisions } from "./collisions/collisions"
+// import { background } from "./classes"
 
 //load & draw canvas
 const canvas = document.querySelector('canvas')
@@ -9,11 +10,8 @@ ctx.fillStyle = 'white'
 ctx.fillRect(0,0,canvas.width,canvas.height)
 
 
-//collision detection 
-const collisionsMap = []
-for(const i=0; i<collisionsMap.length; i+=70){
-    collisionsMap.push(collisions.slice(i, 70+i))
-}
+
+
 
 
 
@@ -80,18 +78,53 @@ window.addEventListener('keyup', (e) => {
 })
 
 
-
+// const movables = [background, testBoundary]
 
 
 //animate game
 function animate(){
     window.requestAnimationFrame(animate)
     background.draw()
-    ctx.drawImage(playerDownImage, 0, 0, playerDownImage.width/4, playerDownImage.height, canvas.width/2 - playerDownImage.width/2, canvas.height/2 - playerDownImage.height/4, playerDownImage.width/4, playerDownImage.height)
+
     
-    if(keys.w.pressed && lastKey==='w') background.position.y += 3
-    else if(keys.s.pressed && lastKey==='s')  background.position.y -= 3
-    else if(keys.a.pressed && lastKey==='a') background.position.x += 3
-    else if(keys.d.pressed && lastKey==='d') background.position.x -= 3
+    //draw boundaries 
+    // boundaries.forEach(boundary => {
+    //     boundary.draw()
+    // })
+
+    testBoundary.draw()
+
+    // ctx.drawImage(playerDownImage, 0, 0, playerDownImage.width/4, playerDownImage.height, canvas.width/2 - playerDownImage.width/2, canvas.height/2 - playerDownImage.height/4, playerDownImage.width/4, playerDownImage.height)
+
+    // if(playerDownImage.position.x + playerDownImage.width)
+    
+    //player mobility 
+    if(keys.w.pressed && lastKey==='w'){
+        movables.forEach((movable) => {
+            movable.position.y += 3
+        })
+
+        // background.position.y += 3
+        // boundaries.position.y += 3
+    } 
+    else if(keys.s.pressed && lastKey==='s'){
+        movables.forEach((movable) => {
+            movable.position.y -= 3
+        })
+        // background.position.y -= 3
+        // boundaries.position.y -= 3
+    }  
+    else if(keys.a.pressed && lastKey==='a'){
+        movables.forEach((movable) => {
+            movable.position.x += 3
+        })
+        // background.position.x += 3
+    } 
+    else if(keys.d.pressed && lastKey==='d'){
+        movables.forEach((movable) => {
+            movable.position.x -= 3
+        })
+        // background.position.x -= 3
+    } 
 }
 animate()
--- classes.js
+++ classes.js
@@ -8,7 +8,7 @@ const playerDownImage = new Image()
 playerDownImage.src = './images/player/playerDown.png'
 
 
-//sprite classes 
+//sprite classe
 class Sprite {
     constructor({ position, velocity, image }) {
         this.position = position
@@ -16,13 +16,75 @@ class Sprite {
     }
     draw(){
         ctx.drawImage(this.image, this.position.x, this.position.y)
+        ctx.drawImage(playerDownImage, 0, 0, playerDownImage.width/4, playerDownImage.height, canvas.width/2 - playerDownImage.width/2, canvas.height/2 - playerDownImage.height/4, playerDownImage.width/4, playerDownImage.height)
     }
 }
 
 const background = new Sprite({
     position: {
-        x: -500,
-        y: -250 
+        x: -425,
+        y: -280 
     },
     image 
 })
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//boundary class for collision detection 
+class Boundary {
+    static width = 48
+    static height = 48 
+    constructor({ position }){
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+    draw(){
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+//collision detection 
+const collisionsMap = []
+for(let i=0; i<collisions.length; i+=70){
+    collisionsMap.push(collisions.slice(i, 70+i))
+}
+
+const boundaries = []
+const offset = {
+    x: -425,
+    y: -280
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if(symbol === 1025)
+        boundaries.push(
+            new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x,
+                    y: i * Boundary.height + offset.y
+                }
+            })
+        )
+    })
+})
+
+const testBoundary = new Boundary({
+    position: {
+        x: 400,
+        y: 400
+    }
+})
+
+
+
+
+
+//movables object for animate in app.js 
+const movables = [background, testBoundary]
